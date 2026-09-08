cf1a4215cb89c226d728c9124815b4071cf175e4
Phạm Ngọc Toàn <ngoctoan.pham@tp.com.vn>
2026-09-08 16:44:58 +0700

    step-1-init-android-project

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.chatapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.chatapp.theme.ChatAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      ChatAppTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
+    }
+  }
+}
