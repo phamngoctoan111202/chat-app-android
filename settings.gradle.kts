e5c35c4e52d774d3d749400720e8a64b7c366bbd
Phạm Ngọc Toàn <ngoctoan.pham@tp.com.vn>
2026-09-09 06:36:15 +0700

    fix: simplify settings.gradle.kts for Android Studio auto-sync

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,10 +25,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 rootProject.name = "ChatApp"
 include(":app")
 include(":core:crypto")
